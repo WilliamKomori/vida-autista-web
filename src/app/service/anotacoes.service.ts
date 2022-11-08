e88d2936d14353278bf61a4834fddb0d7e121ac4
William Mitsuo Komori <tatoow@gmail.com>
2022-11-07 23:04:10 -0300

    chore(baseUrl):changed to heroku backend url.

--- src/app/service/anotacoes.service.ts
+++ src/app/service/anotacoes.service.ts
@@ -10,16 +10,16 @@ export class AnotacoesService {
 
   constructor(public http: HttpClient) { }
 
-  public inserirAnotacao(param: any){
-    return this.http.post("http://localhost:8080/notes/new",param);
+  public inserirAnotacao(param: any) {
+    return this.http.post("https://vida-autista-backend.herokuapp.com/notes/new", param);
   }
 
-  public getAnotacaoByUser(id: number){
-    return this.http.get<Notes[]>("http://localhost:8080/notes/all/"+id);
+  public getAnotacaoByUser(id: number) {
+    return this.http.get<Notes[]>("https://vida-autista-backend.herokuapp.com/notes/all/" + id);
   }
 
-  public getAnotacaoByUserAndFase(id: number, idFase: number){
-    return this.http.get<Notes[]>("http://localhost:8080/notes/all/"+id+"/"+idFase);
+  public getAnotacaoByUserAndFase(id: number, idFase: number) {
+    return this.http.get<Notes[]>("https://vida-autista-backend.herokuapp.com/notes/all/" + id + "/" + idFase);
   }
 
 }
